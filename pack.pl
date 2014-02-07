23d23fac351523b0b0ecb52604490e2763a08c38
Jan Tatham <jan@sebity.com>
2014-02-07 13:38:40 +0000

    Add pack.pl

---
+++ pack.pl
@@ -0,0 +1,8 @@
+name(plOpenGL).
+version('0.5.0').
+title('OpenGL Bindings').
+keywords([opengl,3d,graphics]).
+author( 'Jan Tatham', 'jan@sebity.com' ).
+home('http://www.sebity.com/index.html' ).
+download( 'http://www.sebity.com/downloads/plOpenGL-0.5.0.tgz' ).
+
