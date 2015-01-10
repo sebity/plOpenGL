660001f2c11787b533ca371dea53ea97fab5f2ab
Jan Tatham <jan@sebity.com>
2015-01-10 19:47:34 +0000

    Updated pack config file

--- pack.pl
+++ pack.pl
@@ -1,9 +1,8 @@
 name(plOpenGL).
-version('0.6.1').
+version('0.6.2').
 title('OpenGL Bindings').
-keywords([opengl,graphics]).
+keywords([opengl,graphics,games,bindings]).
 author( 'Jan Tatham', 'jan@sebity.com' ).
-home('http://www.sebity.com/index.html' ).
-download( 'http://www.sebity.com/downloads/plOpenGL-0.6.1.tgz' ).
-download( 'http://www.sebity.com/downloads/plOpenGL-0.6.1.zip' ).
+home('http://www.sebity.com' ).
+download( 'http://www.sebity.com/downloads/plOpenGL-0.6.2.zip' ).
 
